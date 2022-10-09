fbb7d2b3e08edcff1c4ed4bc111f35bdf5b8b7ee
Verousca <vekemamonjowa@gmail.com>
2022-10-09 13:38:57 +0200

    exercises in class

--- 3-functions/1-functions/exercises/3-write-function.js
+++ 3-functions/1-functions/exercises/3-write-function.js
@@ -6,7 +6,10 @@ console.log('-- begin --');
 // write the function to pass the assertions
 // fill in the documentation to describe the function
 
-const scramble = () => {};
+const scramble = (param1, param2,param3) => {
+  retuen param3 + param1
+  +param2;
+};
 
 const _1_expect = 'yxz';
 const _1_actual = scramble('x', 'z', 'y');
@@ -20,4 +23,23 @@ const _3_expect = 'yzx';
 const _3_actual = scramble('z', 'x', 'y');
 console.assert(_3_actual === _3_expect, 'Test 3');
 
+
+
+
+
+function isLegalAge (age){
+  if (age >=18){
+    return true;
+  }else {
+    return false;
+  }
+}
+
+function canDrive(age) {
+  if (isLegalAge(age)) {
+    console.log('you can drive')
+  }else {
+    console.log('you cannot drive')
+  }
+}
 console.log('-- end --');
